2800ef7ad67e00da5ed6872b63b2df7fdec468e5
philtweir <philtweir@users.noreply.github.com>
2022-07-24 22:26:01 +0000

    jekyll build from Action 141d84ee66a2ce7b06fdb57e4745a1345f70abe4

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Caol le caol...",
+        "excerpt":"Interactive DRAFT   This is a draft, it has KNOWN ERRORS.       Standard words in the “Irish UD treebank”, counted with repetition (~91000 words).   (using UD_Irish-IDT)   Inspired by toingaeilge.com  ","categories": ["en","grammar"],
+        "tags": [],
+        "url": "/soirbhiochas/en/grammar/2022/07/10/caol-le-caol.html",
+        "teaser": null
+      },{
+        "title": "Caol le caol...",
+        "excerpt":"DRÉACHT idirghníomhach.   Is dréacht é seo, tá EARRÁIDÍ AITHEANTA aige.       Focail chaighdéanacha sa “Irish UD treebank”, á gcomhaireamh go athrá (~91000 focal).   (ag baint úsáid as UD_Irish-IDT)   Spreagtha ag toingaeilge.com  ","categories": ["ga","grammar"],
+        "tags": [],
+        "url": "/soirbhiochas/ga/grammar/2022/07/10/caol-le-caol.html",
+        "teaser": null
+      },{
+        "title": "Gender breakdown...",
+        "excerpt":"Interactive DRAFT   This is a draft, it has KNOWN ERRORS.       Standard words in the “Irish UD treebank”, counted with repetition (~29000 words).   (using UD_Irish-IDT)   Inspired by Wikibooks Irish  ","categories": ["en","grammar"],
+        "tags": [],
+        "url": "/soirbhiochas/en/grammar/2022/07/24/gender.html",
+        "teaser": null
+      },{
+        "title": "Miondealú inscne...",
+        "excerpt":"DRÉACHT idirghníomhach.   Is dréacht é seo, tá EARRÁIDÍ AITHEANTA aige.       Focail chaighdéanacha sa “Irish UD treebank”, á gcomhaireamh go athrá (~29000 focal).   (ag baint úsáid as UD_Irish-IDT)   Spreagtha ag Wikibooks Irish  ","categories": ["ga","grammar"],
+        "tags": [],
+        "url": "/soirbhiochas/ga/grammar/2022/07/24/gender.html",
+        "teaser": null
+      }]
